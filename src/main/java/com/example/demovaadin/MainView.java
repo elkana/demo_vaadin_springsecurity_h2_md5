fb573ff51ee12172515c906ff0796a7d4eb3ae98
ellkana <ellkana@gmail.com>
2024-01-26 08:36:48 +0700

    contextmenu

--- src/main/java/com/example/demovaadin/MainView.java
+++ src/main/java/com/example/demovaadin/MainView.java
@@ -11,43 +11,86 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.avatar.Avatar;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Hr;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.theme.lumo.Lumo;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
 // https://vaadin.com/docs/latest/application/main-view
 public class MainView extends AppLayout {
+    SecurityService serviceSecurity;
+    Avatar avatar = new Avatar();
 
     // https://vaadin.com/docs/latest/tutorial/login-and-authentication
-    public MainView(@Autowired SecurityService securityService) {
-        if (securityService.getAuthenticatedUser() == null)
+    public MainView(SecurityService serviceSecurity) {
+        if (serviceSecurity.getAuthenticatedUser() == null)
             return;
+        this.serviceSecurity = serviceSecurity;
         H2 logo = new H2("Mobile Collection Monitoring");
+        logo.getStyle().set("font-size", "var(--lumo-font-size-l)")
+                .set("line-height", "var(--lumo-size-l)").set("margin", "0 var(--lumo-space-m)");
 
+        // configure popup menu on avatar
+        var avatarMenu = new ContextMenu();
+        avatarMenu.setTarget(avatar);
+        avatarMenu.setOpenOnClick(true);
+        var subMenuTheme = avatarMenu.addItem("Theme");
+        subMenuTheme.getSubMenu().addItem("Light", click -> changeTheme(false));
+        subMenuTheme.getSubMenu().addItem("Dark", click -> changeTheme(true));
+        avatarMenu.add(new Hr());
+        avatarMenu.addItem("Logout", click -> serviceSecurity.logout());
+
+        // configureheader
         var headerLayout = new HorizontalLayout(new DrawerToggle(), logo,
-                // https://vaadin.com/blog/toggle-dark-lumo-theme-variant-dynamically
-                new Checkbox("Dark", true, e -> {
-                    boolean isDark = e.getSource().getValue().booleanValue();
-                    // e.getSource().setLabel(!isDark?"Switch to light": "Switch to dark");
-                    var js = "document.documentElement.setAttribute('theme', $0)";
-                    getElement().executeJs(js, !isDark ? Lumo.LIGHT : Lumo.DARK);
-                }), new Text(securityService.getUserDetail().getFullName()),
-                // logout
-                new Button("LogOut", click -> securityService.logout()));
+                new Text(serviceSecurity.getUserDetail().getFullName()), avatar);
         headerLayout.setWidthFull();
         headerLayout.expand(logo); // stretch the logo area
         addToNavbar(headerLayout);
 
-        List<Component> menu = new ArrayList<Component>();
-        menu.add(new RouterLink("Dashboard", DashboardView.class));
-        menu.add(new RouterLink("To Do", ToDoView.class));
-        if (securityService.isAdmin())
-            menu.add(new RouterLink("Admin", AdminView.class));
+        // configure drawer
+        var nav = getSideNav();
+        var scroller = new Scroller(nav);
+        scroller.setClassName(LumoUtility.Padding.SMALL);
+        addToDrawer(scroller);
 
-        addToDrawer(new VerticalLayout(menu.toArray(Component[]::new)));
+        // List<Component> menu = new ArrayList<Component>();
+        // menu.add(new RouterLink("Dashboard", DashboardView.class));
+        // menu.add(new RouterLink("To Do", ToDoView.class));
+        // if (serviceSecurity.isAdmin())
+        // menu.add(new RouterLink("Admin", AdminView.class));
+        // addToDrawer(new VerticalLayout(menu.toArray(Component[]::new)));
+    }
+
+    private SideNav getSideNav() {
+        var sideNav = new SideNav();
+        sideNav.addItem(new SideNavItem("Dashboard", "/", VaadinIcon.DASHBOARD.create()),
+                new SideNavItem("Tasks", "/todo", VaadinIcon.LIST.create()));
+        if (serviceSecurity.isAdmin())
+            sideNav.addItem(new SideNavItem("Admin", "/admin", VaadinIcon.RECORDS.create()));
+        // new SideNavItem("Orders", "/orders", VaadinIcon.CART.create()),
+        // new SideNavItem("Customers", "/customers", VaadinIcon.USER_HEART.create()),
+        // new SideNavItem("Products", "/products", VaadinIcon.PACKAGE.create()),
+        // new SideNavItem("Analytics", "/analytics", VaadinIcon.CHART.create()),
+        // );
+        return sideNav;
+    }
+
+    // https://vaadin.com/blog/toggle-dark-lumo-theme-variant-dynamically
+    private void changeTheme(boolean dark) {
+        // e.getSource().setLabel(!isDark?"Switch to light": "Switch to dark");
+        var js = "document.documentElement.setAttribute('theme', $0)";
+        getElement().executeJs(js, dark ? Lumo.DARK : Lumo.LIGHT);
     }
 
 }
